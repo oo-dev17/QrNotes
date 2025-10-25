cfc26641ff0c79dcbee652c34e3b33cc5d7dd9e9
oo-dev17 <costa.pavlis@gmail.com>
2025-10-25 17:10:02 +0200

    pic view working

--- app/src/main/java/com/oo_dev17/qrnotes/FullscreenImageDialog.kt
+++ app/src/main/java/com/oo_dev17/qrnotes/FullscreenImageDialog.kt
@@ -3,22 +3,29 @@ import com.oo_dev17.qrnotes.R
 
 import android.content.Context
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.app.AlertDialog
-import com.bumptech.glide.Glide // Use Glide for image loading
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import java.io.File
 
 
-    class FullscreenImageDialog(context: Context, private val imageFile: File) : Dialog(context) {
+class FullscreenImageDialog(context: Context, private val imageFile: File) : Dialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_fullscreen_image)
 
+        // Make the dialog window fullscreen
+        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+
         val fullscreenImageView: ImageView = findViewById(R.id.fullscreenImageView)!!
+        val fullscreenImageViewText: TextView = findViewById(R.id.fullscreenImageViewText)!!
+        fullscreenImageViewText.text = imageFile.name
 
         // Load the image from the file into the ImageView using Glide
-        Glide.with(context)
+        Glide.with(this.context)
             .load(imageFile)
             .into(fullscreenImageView)
 
